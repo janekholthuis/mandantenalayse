f83da46160d86c18d97c623c0f66335d23df54cd
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-09 12:34:31 +0200

    Embed Fillout form in landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -359,7 +359,13 @@ const LandingPage: React.FC = () => {
               </h3>
               
               {/* Fillout Form Embed */}
-             <div style="width:100%;height:500px;" data-fillout-id="wqXUryyoLZus" data-fillout-embed-type="standard" data-fillout-inherit-parameters data-fillout-dynamic-resize></div><script src="https://server.fillout.com/embed/v1/"></script>
+              <div 
+                style={{width: '100%', height: '500px'}} 
+                data-fillout-id="wqXUryyoLZus" 
+                data-fillout-embed-type="standard" 
+                data-fillout-inherit-parameters 
+                data-fillout-dynamic-resize
+              ></div>
             </div>
           </div>
         </div>
