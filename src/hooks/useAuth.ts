3f0aea5b97f8645fdea9793f3ea4b5aee996c350
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-12 11:11:24 +0200

    Updated useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabase';
 import type { User } from '@supabase/supabase-js';
-import { EmailService } from '../services/emailService';
 
 export const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
