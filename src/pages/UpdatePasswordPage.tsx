4800a0cc6f466aa7a1ecdb859f67135752033959
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-12 15:13:43 +0200

    Updated UpdatePasswordPage.tsx

--- src/pages/UpdatePasswordPage.tsx
+++ src/pages/UpdatePasswordPage.tsx
@@ -1,8 +1,12 @@
+// src/pages/UpdatePasswordPage.tsx
 import React from 'react';
 import UpdatePasswordForm from '../components/auth/UpdatePasswordForm';
+import { useSearchParams } from 'react-router-dom';
 
 const UpdatePasswordPage: React.FC = () => {
-  return <UpdatePasswordForm />;
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get('code') || '';
+  return <UpdatePasswordForm code={code} />;
 };
 
 export default UpdatePasswordPage;
