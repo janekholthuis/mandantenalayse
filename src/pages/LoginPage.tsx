e95fc42defa045012bb91d8878fbe1a1f6ae2918
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-12 14:35:28 +0200

    Updated LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import AuthForm from '../components/auth/AuthForm';
+import toast from 'react-hot-toast';
 
 const LoginPage: React.FC = () => {
-  return <AuthForm mode="login" />;
+  const [params] = useSearchParams();
+
+  useEffect(() => {
+    if (params.get('confirmed') === 'true') {
+      toast.success('E-Mail erfolgreich bestätigt 🎉');
+    }
+
+    if (params.get('passwordUpdated') === 'true') {
+      toast.success('Passwort erfolgreich geändert 🔐');
+    }
+  }, [params]);
+
+  return <AuthForm />;
 };
 
 export default LoginPage;
