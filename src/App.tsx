2a28ed33b0934c73a5c69f5d9379ba0316db1c93
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-12 12:57:34 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-
-import Sidebar from './components/layout/Sidebar';
-import SettingsPage from './pages/SettingsPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ClientsPage from './pages/ClientsPage';
 import NewClientPage from './pages/NewClientPage';
-import ChangeEmailDialog from './components/settings/ChangeEmailDialog';
+import SettingsPage from './pages/SettingsPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 import ConfirmEmailPage from './pages/ConfirmEmailPage';
+import EmailConfirmationSentPage from './pages/EmailConfirmationSentPage';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/layout/Navbar';
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
-      <div className="flex min-h-screen bg-gray-100">
-        <Sidebar />
-        <main className="flex-1 p-6">
-          <Toaster position="bottom-right" />
+      <div className="min-h-screen bg-gray-50">
+        {/* ✅ Navbar global sichtbar */}
+        <Navbar />
+
+        <div className="max-w-7xl mx-auto p-4">
           <Routes>
-            <Route path="/" element={<Navigate to="/clients" replace />} />
+            <Route path="/" element={<Navigate to="/clients" />} />
             <Route path="/clients" element={<ClientsPage />} />
             <Route path="/clients/new" element={<NewClientPage />} />
             <Route path="/settings" element={<SettingsPage />} />
-            <Route path="/confirm-email" element={<ConfirmEmailPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/email-confirmation" element={<ConfirmEmailPage />} />
+            <Route path="/email-confirmation-sent" element={<EmailConfirmationSentPage />} />
           </Routes>
-        </main>
+        </div>
+
+        <Toaster position="top-right" />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
