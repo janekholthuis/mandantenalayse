f94f8c5221f30b81a9e175267295c71cbe4c0088
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-13 13:23:21 +0200

    Updated Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-white border-b border-gray-200 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo - Always on the left */}
           <div className="flex-shrink-0 flex items-center">
