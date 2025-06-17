5270e840d6f440eeece43aa4b1a3e21ecc180393
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-17 21:48:23 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    open: true, // Dev-Server öffnet Browser automatisch
+  }
 });
