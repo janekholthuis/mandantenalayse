c3b1badc186528a9a73563a367d9b2529a09da99
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-18 10:38:31 +0200

    Updated formatters.ts

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -2,7 +2,7 @@ export const formatCurrency = (amount: number): string => {
   return new Intl.NumberFormat('de-DE', {
     style: 'currency',
     currency: 'EUR',
-    minimumFractionDigits: 1,
+    minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(amount);
 };
