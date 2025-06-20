f214ebc7a2e08979533c44b91d1bd77756999e31
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-20 16:18:31 +0200

    Updated ClientDetailPage.tsx

--- src/pages/ClientDetailPage.tsx
+++ src/pages/ClientDetailPage.tsx
@@ -400,7 +400,7 @@ const ClientDetailPage: React.FC = () => {
             <button
               onClick={() => handleTabChange('Mitabeiter')}
               className={`py-4 px-1 border-b-2 font-medium text-sm ${
-                activeTab === 'contracts'
+                activeTab === 'Mitarbeiter'
                   ? 'border-blue-500 text-blue-600'
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               }`}
