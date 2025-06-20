6ad4549c4767adcee7626219bc7e7950b4cee1dd
janekholthuis <j.holthuis@agency-blueprint.io>
2025-06-20 14:57:49 +0200

    Updated ClientSearch.tsx

--- src/components/client/ClientSearch.tsx
+++ src/components/client/ClientSearch.tsx
@@ -22,7 +22,7 @@ const ClientSearch: React.FC<ClientSearchProps> = ({ onSearch }) => {
         <input
           type="text"
           className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm transition duration-150 ease-in-out"
-          placeholder=" Mandanten nach Name, Branche oder Rechtsform suchen..."
+          placeholder=" Mandanten nach Name oder Rechtsform suchen..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
